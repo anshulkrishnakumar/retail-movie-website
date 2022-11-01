158850800c262cd23b3e76f829518fe81f90f865
Mr. Qayum <omar.qayum@tdsb.on.ca>
2022-11-01 13:36:34 -0400

    Completed calculator component

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,21 +1,24 @@
 <script setup>
 import { ref } from "vue";
 
-let operandOne = ref(0);
-let operandTwo = ref(0);
+let operandOne = ref("");
+let operandTwo = ref("");
 let operation = ref("");
 let result = ref(0);
 
 const setOperand = (operand) => {
   if (operation.value === "") {
-    operandOne.value = operand;
+    operandOne.value += operand;
+    result.value = operandOne.value;
   } else {
-    operandTwo.value = operand;
+    operandTwo.value += operand;
+    result.value = operandTwo.value;
   }
 };
 
 const setOperator = (operator) => {
   operation.value = operator;
+  result.value = operator;
 };
 
 const getResult = () => {
@@ -24,7 +27,7 @@ const getResult = () => {
       result.value = operandOne.value - operandTwo.value;
       break;
     case "+":
-      result.value = operandOne.value + operandTwo.value;
+      result.value = parseInt(operandOne.value, 10) + parseInt(operandTwo.value, 10);
       break;
     case "/":
       result.value = operandOne.value / operandTwo.value;
@@ -38,26 +41,34 @@ const getResult = () => {
 
 <template>
   <div class="calculator-container">
-    <label class="display">0</label>
+    <label class="display">{{ result }}</label>
     <button v-for="operand in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]" @click="setOperand(operand)">{{ operand }}</button>
     <button v-for="operator in ['-', '*', '/', '+']" @click="setOperator(operator)">{{ operator }}</button>
     <button @click="getResult()">=</button>
   </div>
-
-  {{ operandOne }}
-  {{ operandTwo }}
-  {{ operation }}
-  {{ result }}
 </template>
 
 
 <style scoped>
 .calculator-container {
   display: grid;
+  width: 50%;
+  gap: 0.25rem;
   grid-template-columns: repeat(5, 1fr);
 }
 
 .calculator-container .display {
   grid-column: 1 / span 5;
+  text-align: right;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  color: white;
+  font-size: 2rem;
+}
+
+.calculator-container button {
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  color: black;
+  font-size: 1.5rem;
+  background-color: grey;
 }
 </style>
